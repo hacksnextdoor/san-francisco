3548adfe7696e2c50d71c800f2c1b905f3d16975
hamt3ch <hmiles23@ufl.edu>
2017-04-01 19:22:40 -0700

    finshed decrement function works

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
-
 export function inc(byN) {
   return {
     type: 'INC',
+    payload: {
+      count: byN
+    }
+  }
+}
+
+export function dec(byN) {
+  return {
+    type: 'DEC',
     payload: {
       count: byN
     }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import store from './store'
 
-import { inc } from './actions'
+import { inc, dec } from './actions'
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
@@ -12,8 +12,9 @@ store.subscribe(() => {
 store.dispatch(inc(5))
 store.dispatch(inc(1))
 store.dispatch(inc(2))
-store.dispatch({type: "DEC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "DEC"})
+
+store.dispatch(dec(2))
+store.dispatch(dec(1))
+store.dispatch(dec(5))
 
 ReactDOM.render(<App />, document.getElementById('root'));
